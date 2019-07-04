fdb340e286b2646a3baa56bc823e6c4aa73d69ef
Jorge Garcia <jlg.sftdev@gmail.com>
2019-07-03 19:51:58 -0700

    Connected app to MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.json({ msg: 'Yoo..' }));
 
+// All possible routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
