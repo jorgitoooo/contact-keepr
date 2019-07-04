e73e43f401db6f9b90d008be6a7f826a36b85ef7
Jorge Garcia <jlg.sftdev@gmail.com>
2019-07-03 23:11:49 -0700

    Added get user functionality

--- routes/auth.js
+++ routes/auth.js
@@ -6,12 +6,22 @@ const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
+const authMiddleware = require('../middleware/auth');
+
 const router = express.Router();
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', async (req, res) => {});
+router.get('/', authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
 
 // @route   POST api/auth
 // @desc    Auth user & get token
