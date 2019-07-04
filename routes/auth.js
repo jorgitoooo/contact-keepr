e4b2db995389bd127d47a743dc1d91cfa2ba9a48
Jorge Garcia <jlg.sftdev@gmail.com>
2019-07-03 22:34:37 -0700

    Added registration and login/authentication functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,70 @@
 const express = require('express');
+const config = require('config');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 const router = express.Router();
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
-});
+router.get('/', async (req, res) => {});
 
 // @route   POST api/auth
 // @desc    Auth user & get token
 // @access  Public
-router.post('/', (req, res) => {
-  res.send('Log in user');
-});
+router.post(
+  '/',
+  [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a password').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ msg: 'User with email ' + email + ' does not exist' });
+      }
+
+      if (!bcrypt.compareSync(password, user.password)) {
+        return res.status(400).json({ msg: 'Incorrect password' });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const app = express();
 // Connect database
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+// Init middleware (accepts request body data)
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.json({ msg: 'Yoo..' }));
 
@@ -14,5 +15,7 @@ app.get('/', (req, res) => res.json({ msg: 'Yoo..' }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('Server listening to port : ', PORT));
